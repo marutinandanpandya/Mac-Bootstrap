71cacb3b972838c4f85a6966f49ded06c08e7456
marutinandanpandya <marut>
2016-06-12 02:21:55 +0530

    Adding few .dotfiles

---
+++ dotfiles/bootstap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+FILES=(.aliases  .extra .functions .gitconfig .vimrc .gemrc)
+
+for i in "${FILES[@]}"
+do
+	FILE=${i}
+	PATH=${BASE_DIR}/${FILE}
+	HOME_PATH=${HOME}/${FILE}
+	echo "Mapping ${PATH} to ${HOME_PATH}"
+	/bin/rm -Rf ${HOME_PATH}
+	/bin/ln -s ${PATH} ${HOME_PATH}
+done
+
+export PATH=/bin:/usr/bin:${PATH}
+
+
+echo "Done."
