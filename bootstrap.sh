eaa86044b59791feaf15760831e84fdcccd1aacf
Maruti Nandan Pandya <marutinandan.pandya@coupa.com>
2020-06-10 08:26:15 +0530

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,18 @@
 
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew bundle
+
+docker-machine create --driver virtualbox default
+docker-machine env default
+docker-machine stop default
+
+xcode-select --install
+xcrun simctl delete unavailable
+
+
+
 FILES=(.aliases .bashrc .profile .bash_logout .bash_profile .bash_prompt .extra .functions .gitconfig .vimrc .gemrc)
 
 for i in "${FILES[@]}"
@@ -13,8 +25,5 @@ do
 	/bin/rm -Rf ${HOME_PATH}
 	/bin/ln -s ${PATH} ${HOME_PATH}
 done
-
-export PATH=/bin:/usr/bin:${PATH}
-
 
 echo "Done."
